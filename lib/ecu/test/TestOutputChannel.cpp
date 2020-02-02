0f20151654905cf528044ec647f959fa3aa326fe
Matthew Kennedy <matthewkennedy@outlook.com>
2020-02-02 14:22:23 -0800

    An actual test!

--- lib/ecu/test/TestOutputChannel.cpp
+++ lib/ecu/test/TestOutputChannel.cpp
@@ -1,6 +1,22 @@
 #include <gtest/gtest.h>
 
+#include "channels/OutputChannel.h"
+
+using namespace ecu;
+
+template <class T>
+Span<uint8_t> Pun(T& val)
+{
+	return Span(reinterpret_cast<uint8_t*>(&val), sizeof(val));
+}
+
 TEST(OutputChannel, FloatUnscaled)
 {
+	ScaledOutputChannel<float> ch("test", "Test Channel", { 0, 100 }, 1.0f, 0.0f);
 
+	float testValue = 123.45;
+
+	ch.PostBytes(Pun(testValue));
+
+	EXPECT_EQ(ch.GetValue(), testValue);
 }
