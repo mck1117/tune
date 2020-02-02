d7200ba5e0cda2e84cf66762551c70202b3feb00
Matthew Kennedy <matthewkennedy@outlook.com>
2020-02-01 17:03:04 -0800

    add TS retry

--- lib/ecu/src/interface/TunerstudioProtocol.cpp
+++ lib/ecu/src/interface/TunerstudioProtocol.cpp
@@ -1,6 +1,28 @@
 
 #include "TunerstudioProtocol.h"
 
+enum class TsResponseCode : uint8_t
+{
+	Ok = 0x00,
+	PageData = 0x02,
+	ConfigError = 0x03,
+	BurnOk = 0x04,
+
+	// errors
+	Underrun = 0x80,
+	Overrun = 0x81,
+	CrcFailure = 0x82,
+	UnrecognizedCommand = 0x83,
+	OutOfRange = 0x84,
+	Busy = 0x85,
+	FlashLocked = 0x86,
+};
+
+bool IsSuccess(TsResponseCode code)
+{
+	return code < TsResponseCode::Underrun;
+}
+
 namespace ecu
 {
 
@@ -8,11 +30,21 @@ std::vector<uint8_t> TunerstudioEcuInterface::GetOutputChannelBuffer()
 {
 	std::vector<uint8_t> cmd{ 0, 5, 'O', 0, 0, 0xdc, 0 };
 
-	auto result = m_protocol.DoTransactionCrc(cmd);
+	for(int retries = 3; retries > 0; retries--)
+	{
+		auto result = m_protocol.DoTransactionCrc(cmd);
 
-	// first byte is status
-	result.erase(result.begin());
+		auto status = static_cast<TsResponseCode>(result[0]);
+		if (IsSuccess(status))
+		{
+			// Erase status byte
+			result.erase(result.begin());
+			return result;
+		}
+	}
 
-	return result;
+	// if we got here, we failed all retries
+	// todo: handle properly
+	throw "";
 }
 }
